22cd686807e8b052da1c947f0c178345bff8d53b
Santhosh-dukey <santhoshgunaseelan9159@gmail.com>
2023-08-22 21:52:09 +0530

    added location path color

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -2,8 +2,10 @@ import { navbar } from '@/src/Components/Array';
 import { useRouter } from 'next/router';
 import React from 'react'
 import 'animate.css';
+import { Box } from '@mui/material';
 function Navbar() {
     const router = useRouter()
+    const location = router.pathname
   return (
     <>
       <div className=" justify-between items-center flex flex-row ">
@@ -11,16 +13,17 @@ function Navbar() {
             <div className="flex flex-col w-full h-full justify-arround pt-20 animate__animated animate__fadeInRight">
               {navbar.map((item, id) => {
                 return (
-                  <div
+                  <Box
+                 
                     key={id}
                     onClick={() => {
                       router.push(item.to);
                     }}
                     className="cursor-pointer hover:scale-110 text-white transistion w-full flex flex-row gap-2 w-35 h-16 items-center justify-between "
                   >             
-                  <div > {item.title}</div>     
+                  <Box  sx={{color: location == item.to ? "#fff" : "#4d4d4d"}} > {item.title}</Box>     
                     <div className='text-blue-500 text-2xl'>{item.icon}</div>
-                  </div>
+                  </Box>
                 );
               })}
             </div>
